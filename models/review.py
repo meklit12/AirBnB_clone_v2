072d78ac74c3fda37f2ffa2e9402963c9a5b86f2
root <fraol123>
2021-08-11 06:58:32 +0000

    check

--- models/review.py
+++ models/review.py
@@ -12,7 +12,7 @@ class Review(BaseModel, Base):
     if models.storage_t == 'db':
         __tablename__ = "reviews"
         place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-        user_id = Column(String(60), ForeignKey("user.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         text = Column(String(1024), nullable=False)
 
     else:
